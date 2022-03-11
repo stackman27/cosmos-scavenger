8e9547552894e6caa24132c5244fa3c929a1d7a9
stackman27 <sis1001@berkeley.edu>
2022-03-10 18:15:15 -0800

    add scavenge messages

---
+++ x/scavenge/keeper/msg_server_submit_scavenge.go
@@ -0,0 +1,17 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/cosmonaut/scavenge/x/scavenge/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) SubmitScavenge(goCtx context.Context, msg *types.MsgSubmitScavenge) (*types.MsgSubmitScavengeResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Handling the message
+	_ = ctx
+
+	return &types.MsgSubmitScavengeResponse{}, nil
+}
